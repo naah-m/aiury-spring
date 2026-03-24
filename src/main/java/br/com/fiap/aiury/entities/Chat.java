40ebffd77d15c9d5f49ff22406be4f8e1edc6aea
rafael051 <rafael.cooperativas@gmail.com>
2026-03-24 11:18:53 -0300

    ajustar mapeamento de Chat e remover @Temporal de LocalDateTime

--- src/main/java/br/com/fiap/aiury/entities/Chat.java
+++ src/main/java/br/com/fiap/aiury/entities/Chat.java
@@ -8,32 +8,28 @@ import java.util.List;
 
 @Data
 @Entity
-//@Table(name = "chat")
 public class Chat {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    //@Column(name = "id_chat")
     private Long id;
 
     @ManyToOne
-    //@JoinColumn(name = "id_usuario", nullable = false)
+    @JoinColumn(name = "id_usuario", nullable = false)
     private Usuario usuario;
 
     @ManyToOne
-    //@JoinColumn(name = "id_ajudante")
+    @JoinColumn(name = "id_ajudante")
     private Ajudante ajudante;
 
-    @Temporal(TemporalType.TIMESTAMP)
-    //@Column(name = "data_inicio", nullable = false)
+    @Column(name = "data_inicio", nullable = false)
     private LocalDateTime dataInicio;
 
-    @Temporal(TemporalType.TIMESTAMP)
-    //@Column(name = "data_fim")
+    @Column(name = "data_fim")
     private LocalDateTime dataFim; // Nullable conforme o modelo conceitual (data_fim)
 
     @Enumerated(EnumType.STRING)
-    //@Column(name = "status", nullable = false)
+    @Column(name = "status", nullable = false)
     private ChatStatus status;
 
     @OneToMany(mappedBy = "chat")
